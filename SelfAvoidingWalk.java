e5a5cfff07a7a6469c73efcd74c3d2228d012e58
tammy vu <tammy.vsk@gmail.com>
2023-02-23 12:11:46 -0600

    testing path averages

--- SelfAvoidingWalk.java
+++ SelfAvoidingWalk.java
@@ -1,18 +1,16 @@
 public class SelfAvoidingWalk {
 
-    public static void printPathLengths(int n, int nTrials) { 
-        //print average length of paths 
-        //(seperate dead and escape)
-        //dead end probality
+    public static boolean oneWalk(int n) {
         boolean[][] grid = new boolean[n][n];
-        int x = n/2; //center starting position
-        int y = n/2;
-        int nEscapes = 0;
-        int nDeads = 0;
 
-        for(int trials = 0; trials < nTrials; trials++){
+            int x = n/2; //center starting position
+            int y = n/2;
+           
+           // int pathLength = 0;
+
             while(x >= 0 && x <n && y >=0 && y < n && grid[x][y] == false){//condition if path is out in bounds or hasnt run into itself
                 grid[x][y] = true; 
+               // pathLength++;
                 double r = Math.random();
                 if (r < .25){//up
                     y--; // remember: (0,0) starts top left, oposite traversing 
@@ -24,24 +22,35 @@ public class SelfAvoidingWalk {
                     x++;
                 }
             }
-    
             if (x < 0 || x >= n || y < 0 || y >= n){//case escape
-                nEscapes++;
+                return true;
+            }else{ //case dead end
+                return false;
             }
-            if (grid[x][y] == true){//case dead ends
+    }
+
+    public static void printPathLengths(int n, int nTrials) { 
+        //print average length of paths 
+        //(seperate dead and escape)
+        //dead end probality
+        int nEscapes = 0;
+        int nDeads = 0;
+
+        for(int trials = 0; trials < nTrials; trials++){
+            if (oneWalk(n)){
+                nEscapes++;
+            }else{
                 nDeads++;
             }
         }
-
-        
-
-        System.out.println();
-
+        System.out.println("dead end probability: " + (double)nDeads/nTrials);
+           
     }
+    
 
     public static void main(String[] args) {
         int n = 5;
-        int nTrials = 1000;
+        int nTrials = 1000000;
 
         printPathLengths(n, nTrials);
     }
