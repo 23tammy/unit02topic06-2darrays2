ef662eaa4165be77cf593169d1febaad6bc3ba82
tammy vu <tammy.vsk@gmail.com>
2023-02-24 12:19:01 -0600

    coordinate tracking

--- SelfAvoidingWalk.java
+++ SelfAvoidingWalk.java
@@ -1,13 +1,10 @@
 public class SelfAvoidingWalk {
-/* 
     public static boolean oneWalk(int n) {
         boolean[][] grid = new boolean[n][n];
 
             int x = n/2; //center starting position
             int y = n/2;
            
-           // int pathLength = 0;
-
             while(x >= 0 && x <n && y >=0 && y < n && grid[x][y] == false){//condition if path is out in bounds or hasnt run into itself
                 grid[x][y] = true; 
                // pathLength++;
@@ -27,7 +24,7 @@ public class SelfAvoidingWalk {
             }else{ //case dead end
                 return false;
             }
-    }*/
+    }
 
     public static void printPathLengths(int n, int nTrials) { 
         //print average length of paths 
@@ -68,7 +65,53 @@ public class SelfAvoidingWalk {
         System.out.println("dead end probability: " + (double)nDeads/nTrials + "\naverage length of escape paths: " + ((double)sumEscapeLengths/nTrials) + "\naverage length of dead-end paths: " + ((double)sumDeadLengths/nTrials));
     }
 
-    public static double deadEndRectangleArea(int n, int nTrials) {
+    public static void deadEndRectangleArea(int n, int nTrials) {
+        //take ntrials and nside; return avg area of each path
+        //find furthest right, left, up , and down values;;;keep track of coords
+        int nDeads = 0;
+
+        int x1 = n;
+        int x2 = 0;
+        int y1 = n;
+        int y2 = 0;
+
+
+        for(int trials = 0; trials < nTrials; trials++){
+            boolean[][] grid = new boolean[n][n];
+
+            int x = n/2; //center starting position
+            int y = n/2;
+
+            while(x >= 0 && x <n && y >=0 && y < n && grid[y][x] == false){//condition if path is out in bounds or hasnt run into itself
+                grid[y][x] = true; 
+                double r = Math.random();
+                if (r < .25){//up
+                    y--; // remember: (0,0) starts top left, oposite traversing 
+                    if (y < y1){ //furthest up
+                        y1 = y;
+                    }
+                }else if (r < .5) {//down
+                    y++;
+                    if (y > y2){//furthest down
+                        y2 = y;
+                    }
+                }else if (r < .75) {//left
+                    x--;
+                    if (x < x1){//furthest left
+                        x1 = x;
+                    }
+                }else{//right
+                    x++;
+                    if (x > x2){//furthest right
+                        x2 = x;
+                    }
+                }
+            }
+            if (!(x < 0 || x >= n || y < 0 || y >= n)){
+                nDeads++;
+            }
+        
+        }
         
     }
 
@@ -79,7 +122,8 @@ public class SelfAvoidingWalk {
         int n = 5;
         int nTrials = 1000000;
 
-        printPathLengths(n, nTrials);
+        //printPathLengths(n, nTrials);
+        deadEndRectangleArea(n, nTrials);
         
     }
 }
