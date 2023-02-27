9f7c32128267dd68f700a478964201e7aca99832
tammy vu <tammy.vsk@gmail.com>
2023-02-27 12:06:43 -0600

    something

--- DiceRolls.java
+++ DiceRolls.java
@@ -1,4 +1,18 @@
 public class DiceRolls {
+
+    public static void twoD6Probabilities() {
+        int[] frequencies = new int[13];
+        for (int i = 1; i <= 6; i++) {
+            for (int j = 1; j <= 6; j++) {
+            frequencies[i+j]++;
+            }
+        }
+
+        double[] probabilities = new double[13];
+        for (int k = 1; k <= 12; k++) {
+            probabilities[k] = frequencies[k] / 36.0;
+        }
+    }
     public static void main(String[] args) {
         
     }
